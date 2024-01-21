dda4c8ab825663604c85371385815a6eb49bb88c
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-21 21:32:26 +0100

    Fix parse

--- async-zero-cost-templating-proc-macro2/src/parse.rs
+++ async-zero-cost-templating-proc-macro2/src/parse.rs
@@ -1,3 +1,4 @@
+use proc_macro2_diagnostics::Diagnostic;
 use syn::{braced, parse::Parse, token::Brace, Block, Expr, LitStr, Token};
 
 pub struct HtmlChildren {
@@ -6,9 +7,16 @@ pub struct HtmlChildren {
 
 impl Parse for HtmlChildren {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let span = input.span();
+
         let mut children = Vec::new();
         while !input.is_empty() && !(input.peek(Token![<]) && input.peek2(Token![/])) {
-            children.push(input.parse()?);
+            let child_start_span = input.span();
+            children.push(input.parse().map_err(|err| {
+                Diagnostic::from(err)
+                    .span_note(child_start_span, "while parsing child")
+                    .span_note(span, "while parsing children")
+            })?);
         }
         Ok(Self { children })
     }
@@ -22,10 +30,13 @@ pub enum HtmlChild {
 
 impl Parse for HtmlChild {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let span = input.span();
         if input.peek(LitStr) {
             Ok(Self::Literal(input.parse()?))
         } else if input.peek(Token![if]) {
-            Ok(Self::If(input.parse()?))
+            Ok(Self::If(input.parse().map_err(|err| {
+                Diagnostic::from(err).span_note(span, "while parsing if")
+            })?))
         } else {
             Ok(Self::Computed(input.parse()?))
         }
@@ -41,16 +52,34 @@ pub struct HtmlIf {
 
 impl Parse for HtmlIf {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let content;
         Ok(HtmlIf {
             if_token: input.parse()?,
-            cond: input.parse()?,
-            then_branch: (braced!(content in input), content.parse()?),
+            cond: {
+                let span = input.span();
+                input.call(Expr::parse_without_eager_brace).map_err(|err| {
+                    Diagnostic::from(err).span_note(span, "while parsing if condition")
+                })?
+            },
+            then_branch: {
+                let content;
+                (braced!(content in input), {
+                    let then_span = content.span();
+                    content.parse().map_err(|err| {
+                        Diagnostic::from(err).span_note(then_span, "while parsing then branch")
+                    })?
+                })
+            },
             else_branch: {
                 if input.peek(Token![else]) {
                     Some({
                         let content;
-                        (input.parse()?, braced!(content in input), content.parse()?)
+                        (input.parse()?, braced!(content in input), {
+                            let else_span = content.span();
+                            content.parse().map_err(|err| {
+                                Diagnostic::from(err)
+                                    .span_note(else_span, "while parsing else branch")
+                            })?
+                        })
                     })
                 } else {
                     None
