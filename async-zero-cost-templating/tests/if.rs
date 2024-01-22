0095920efa61b5b8ee6314018fd99fe109f6e702
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-22 15:56:32 +0100

    wip

--- async-zero-cost-templating/tests/if.rs
+++ async-zero-cost-templating/tests/if.rs
@@ -1,8 +1,11 @@
 use async_zero_cost_templating_proc_macro::html_proc_macro;
+use bytes::Bytes;
 
 pub fn main() {
+    let condition = true;
+    let variable = Bytes::from_static(b"hi");
     html_proc_macro! {
-        if test {
+        if condition {
             "true"
             { variable }
         }
--- async-zero-cost-templating/tests/if_else.rs
+++ async-zero-cost-templating/tests/if_else.rs
@@ -2,7 +2,7 @@ use async_zero_cost_templating_proc_macro::html_proc_macro;
 
 pub fn main() {
     html_proc_macro! {
-        if test {
+        if condition {
             "true"
             { variable }
         } else {
