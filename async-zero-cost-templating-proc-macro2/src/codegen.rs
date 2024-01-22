b947d6b8145dfe70f4dc6db354caa9c28a10416d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-22 15:56:35 +0100

    wip

--- async-zero-cost-templating-proc-macro2/src/codegen.rs
+++ async-zero-cost-templating-proc-macro2/src/codegen.rs
@@ -6,12 +6,19 @@ use proc_macro2::Literal;
 use quote::{quote, quote_spanned};
 use syn::spanned::Spanned;
 
+pub fn top_level(input: Vec<Intermediate>) -> proc_macro2::TokenStream {
+    let inner = codegen(input);
+    quote! {
+        let _ = |stream: ::async_zero_cost_templating::FutureToStream| async move {
+            #inner
+        };
+    }
+}
+
 pub fn codegen(input: Vec<Intermediate>) -> proc_macro2::TokenStream {
     let inner = input.into_iter().map(codegen_intermediate);
     quote! {
-        let _ = |stream: ::async_zero_cost_templating::FutureToStream| async move {
-            #(#inner)*
-        };
+        #(#inner)*
     }
 }
 pub fn codegen_intermediate(input: Intermediate) -> proc_macro2::TokenStream {
@@ -24,8 +31,14 @@ pub fn codegen_intermediate(input: Intermediate) -> proc_macro2::TokenStream {
         }
         Intermediate::Computed(computed) => {
             let span = computed.span();
-            quote_spanned! {span=>
-                stream._yield(#computed).await;
+            if let [value] = &computed.stmts[..] {
+                quote_spanned! {span=>
+                    stream._yield(#value).await;
+                }
+            } else {
+                quote_spanned! {span=>
+                    stream._yield(#computed).await;
+                }
             }
         }
         Intermediate::If(HtmlIf {
