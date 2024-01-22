857e4c59032e7cb9edeba4abbc82e3951ce65825
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-22 15:28:19 +0100

    jo

--- async-zero-cost-templating-proc-macro/src/lib.rs
+++ async-zero-cost-templating-proc-macro/src/lib.rs
@@ -1,9 +1,15 @@
-use async_zero_cost_templating_proc_macro2::{codegen::codegen, parse::HtmlChildren};
+use async_zero_cost_templating_proc_macro2::{
+    codegen::codegen,
+    intermediate::{simplify, Intermediate},
+    parse::HtmlChildren,
+};
 use syn::parse_macro_input;
 
 #[proc_macro]
 pub fn html_proc_macro(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let html_children = parse_macro_input!(input as HtmlChildren);
+    let intermediate = Vec::<Intermediate>::from(html_children);
+    let intermediate = simplify(intermediate);
 
-    codegen(html_children).into()
+    codegen(intermediate).into()
 }
--- async-zero-cost-templating-proc-macro2/src/codegen.rs
+++ async-zero-cost-templating-proc-macro2/src/codegen.rs
@@ -1,7 +1,7 @@
-use crate::parse::HtmlChildren;
+use crate::intermediate::Intermediate;
 use quote::quote;
 
-pub fn codegen(_input: HtmlChildren) -> proc_macro2::TokenStream {
+pub fn codegen(_input: Vec<Intermediate>) -> proc_macro2::TokenStream {
     quote! {
         let _ = |stream: ::async_zero_cost_templating::FutureToStream| async move {
             stream._yield(1).await;
