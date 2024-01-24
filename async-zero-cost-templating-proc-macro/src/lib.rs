385a04b9250ffeed607a156f868bce2da0049f0f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-24 01:10:47 +0100

    wip

--- async-zero-cost-templating-proc-macro/src/lib.rs
+++ async-zero-cost-templating-proc-macro/src/lib.rs
@@ -7,7 +7,7 @@ use syn::parse_macro_input;
 
 #[proc_macro]
 pub fn html_proc_macro(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let html_children = parse_macro_input!(input as HtmlChildren);
+    let html_children = MyParse::<HtmlChildren>::my_parse(input);
     let intermediate = Vec::<Intermediate>::from(html_children);
     let intermediate = simplify(intermediate);
 
