702dc6f96fd38e6438aa9b6c2af612decf4c50c4
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-25 21:38:10 +0100

    wip

--- async-zero-cost-templating/tests/for.rs
+++ async-zero-cost-templating/tests/for.rs
@@ -1,7 +1,8 @@
 use async_zero_cost_templating_proc_macro::html_proc_macro;
+use bytes::Bytes;
 
 pub fn main() {
-    let result = futures_util::stream::iter([1, 2, 3]);
+    let mut result = futures_util::stream::iter([Bytes::from_static(b"hi"), Bytes::from_static(b"hi"), Bytes::from_static(b"hi")]);
     html_proc_macro! {
         for row in &mut result {
             "true"
