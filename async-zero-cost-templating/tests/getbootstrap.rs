ae2ba065b1bca62f09fdb941cfa043cdb54ed631
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-26 01:18:29 +0100

    epic

--- async-zero-cost-templating/tests/getbootstrap.rs
+++ async-zero-cost-templating/tests/getbootstrap.rs
@@ -7,19 +7,34 @@ use std::io::Write;
 
 #[tokio::test]
 async fn test() {
-    let value = Bytes::from_static(b"hi");
+    let title = Bytes::from_static(b"Bootstrap demo");
+    let mut result = futures_util::stream::iter([
+        Bytes::from_static(b"abc"),
+        Bytes::from_static(b"def"),
+        Bytes::from_static(b"ghi"),
+    ]);
+    let greet = true;
     let stream = html_proc_macro! {
         <!doctype html>
         <html lang="en">
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>"Bootstrap demo"</title>
+            <title>{title}</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         </head>
         <body>
-            <h1>"Hello, world!"</h1>
+            <h1>
+                if greet {
+                    "Hello, world!"
+                } else {
+                    "Bye, world!"
+                }
+            </h1>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+            for row in &mut result {
+                { row }
+            }
         </body>
         </html>
     };
