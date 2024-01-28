4739c0e2897c5acb231549e78325a7a7ee14a9f7
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-01-28 23:45:25 +0100

    wip

---
+++ async-zero-cost-templating/tests/element_bug_1.rs
@@ -0,0 +1,20 @@
+use async_zero_cost_templating::html;
+use async_zero_cost_templating::TheStream;
+use bytes::Bytes;
+use core::pin::pin;
+use futures_util::stream::StreamExt;
+use std::io::Write;
+
+#[tokio::test]
+async fn test() {
+    let condition = false;
+    let stream = html! {
+        <label for="title">"Title:"</label>
+    };
+    let mut stream = pin!(TheStream::new(stream));
+    let mut stdout = std::io::stdout().lock();
+    while let Some(element) = stream.next().await {
+        stdout.write_all(&element).unwrap();
+    }
+    stdout.write_all(b"\n").unwrap();
+}
